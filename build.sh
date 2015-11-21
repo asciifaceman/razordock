6a432d16cfc7f1aac44d0ce2eee4238a867ea484
Charles C <nafredy@gmail.com>
2015-11-20 21:03:11 -0800

    THERE IS PROBABLY A BETTER WAY TO DO THIS?

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ VERSION=$1
 
 docker build -t ${IMAGE}:${VERSION} . | tee build.log || exit 1
 ID=$(tail -1 build.log | awk '{print $3;}')
+docker rmi ${IMAGE}:latest
 docker tag $ID ${IMAGE}:latest
 
 docker images | grep ${IMAGE}
